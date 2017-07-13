53c553c967b060bc90f0dd0eaa9ce9344b6663bd
Ballak-T <ballakt@students.zcu.cz>
2017-07-13 15:11:35 +0200

    Article Eniti adding

--- app/model/Article.php
+++ app/model/Article.php
@@ -6,9 +6,53 @@
  * Date: 13.7.17
  * Time: 13:03
  */
-class Article extends \Kdyby\Doctrine\Entities\BaseEntity
+
+namespace App;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+
+class Article
 {
 
-        
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+
+    protected $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+
+    protected $idUser;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+
+    protected $title;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+
+    protected $topic;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+
+    protected $content;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+
+    protected $created;
 
 }
